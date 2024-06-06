941a36fe6c09afddcb3334cd66c8361cb4de9379
Raza Sayed <raza.sayed@gmail.com>
2024-06-07 02:08:46 +0530

    wip

--- bot.py
+++ bot.py
@@ -9,27 +9,27 @@ env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events', app)
+slack_event_adapter = SlackEventAdapter(os.environ["SIGNING_SECRET"], '/slack/events', app)
 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
-BOT_ID = client.api_call("auth.test")['user_id']
+client = slack.WebClient(token=os.environ["SLACK_TOKEN"])
+BOT_ID = client.api_call("auth.test")["user_id"]
 
 @slack_event_adapter.on('message')
 def message(payload):
     print(payload)
-    event = payload.get('event', {})
-    channel_id = event.get('channel')
-    user_id = event.get('user')
-    text = event.get('text')
+    event = payload.get("event", {})
+    channel_id = event.get("channel")
+    user_id = event.get("user")
+    text = event.get("text")
     if user_id != BOT_ID:
         client.chat_postMessage(text=f"You said {text}", channel=channel_id)
 
 # /weather is a slash command created in the bot. It can be used as /weather pune for example.
-@app.route('/weather', methods=['POST'])
+@app.route("/weather", methods=['POST'])
 def weather():
     data = request.form
     location = data["text"]
-    return Response(f"The current weather in {location} is 24 degrees", mimetype='text/plain'), 200
+    return Response(f"The current weather in {location} is 24 degrees", mimetype="text/plain"), 200
 
 
 if __name__ == "__main__":
