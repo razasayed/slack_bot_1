07bdd3641595f296231fcca00ad4278ed99f4376
Raza Sayed <raza.sayed@gmail.com>
2024-06-07 02:11:30 +0530

    wip

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ env_path = Path(".") / ".env"
 load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(os.environ["SIGNING_SECRET"], '/slack/events', app)
+slack_event_adapter = SlackEventAdapter(os.environ["SIGNING_SECRET"], "/slack/events", app)
 
 client = slack.WebClient(token=os.environ["SLACK_TOKEN"])
 BOT_ID = client.api_call("auth.test")["user_id"]
